a348edb3d9bc141525fe1ff8c986a62bc8b08587
CyEt55 <albertz_2011@hotmail.com>
2023-05-15 09:40:08 -0500

    mistake

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item.component';
 import { MediaItemListComponent } from './media-item-list.component';
-import { FavoriteDirective } from './favorite.directive';
 
 @NgModule({
   imports: [
@@ -12,8 +11,7 @@ import { FavoriteDirective } from './favorite.directive';
   declarations: [
     AppComponent,
     MediaItemComponent,
-    MediaItemListComponent,
-    FavoriteDirective
+    MediaItemListComponent
   ],
   bootstrap: [
     AppComponent
