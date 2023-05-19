8b861a4da14fc67a2929264ae70315b16b3abf1e
CyEt55 <albertz_2011@hotmail.com>
2023-05-19 12:17:52 -0500

    fix on 07_07b

--- src/app/new-item/new-item.module.ts
+++ src/app/new-item/new-item.module.ts
@@ -1,1 +1,16 @@
 import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { MediaItemFormComponent } from "./media-item-form.component";
+import { ReactiveFormsModule } from "@angular/forms";
+
+@NgModule({
+    imports: [
+        CommonModule,
+        ReactiveFormsModule
+    ],
+    declarations: [
+        MediaItemFormComponent
+    ]
+})
+
+export class NewItemModule{}
